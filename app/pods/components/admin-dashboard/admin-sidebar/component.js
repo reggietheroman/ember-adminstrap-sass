446400efe9539e93c2692c1a669c7e1aaef9d47e
Reggie <reggietheroman@gmail.com>
2017-05-16 20:30:35 +0800

    added admin dashboard styles and templates.

--- app/pods/components/admin-dashboard/admin-sidebar/component.js
+++ app/pods/components/admin-dashboard/admin-sidebar/component.js
@@ -1,4 +1,57 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  model() {
+    let navs = {
+      navGroupA: {
+        nav: [
+          {
+            name: 'navA1',
+            url: '#'
+          },
+          {
+            name: 'navA1',
+            url: '#'
+          },
+          {
+            name: 'navA1',
+            url: '#'
+          }
+        ]
+      },
+      navGroupB: {
+        nav: [
+          {
+            name: 'navB1',
+            url: '#'
+          },
+          {
+            name: 'navB2',
+            url: '#'
+          },
+          {
+            name: 'navB3',
+            url: '#'
+          }
+        ]
+      },
+      navGroupC: {
+        nav: [
+          {
+            name: 'navC1',
+            url: '#'
+          },
+          {
+            name: 'navC2',
+            url: '#'
+          },
+          {
+            name: 'navC3',
+            url: '#'
+          }
+        ]
+      }
+    }
+    return navs;
+  }
 });
